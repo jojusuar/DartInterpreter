bb02d6fca225eb0b76c5c8ff2a6a0470762acaad
Niariasve <nestorariasve@gmail.com>
2024-06-24 17:52:16 -0500

    classDeclaration a Instruction

--- sintactico.py
+++ sintactico.py
@@ -36,6 +36,7 @@ def p_instruction(p):
                 | variableDeclarationUninitialized
                 | variableDeclarationInitialized
                 | variableMutation
+                | classDeclaration
                 | import
     '''
 
